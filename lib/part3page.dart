ca3fba10446ecf09bd3eedaf053ede4acbe4c6be
GY102912 <qaz102912@gmail.com>
2023-12-09 03:55:45 +0900

    add plash screen & fonts

---
+++ lib/part3page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'SchedulePage.dart';
+
+class part3page extends StatefulWidget {
+  const part3page({super.key});
+
+  @override
+  State<part3page> createState() => _part3pageState();
+}
+
+class _part3pageState extends State<part3page> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          child: AppBar(),
+          preferredSize: Size.fromHeight(0),
+        ),
+        /*
+        appBar: AppBar(
+          title: const Text('독서 일정 관리'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: '독서 일정'),
+              //Tab(text: '임시',)
+            ],
+          ),
+        ),
+         */
+        body: const TabBarView(
+          children: [
+            SchedulePage(),
+            //SuspendedBookList()
+          ],
+        ),
+      ),
+    );
+  }
+}
